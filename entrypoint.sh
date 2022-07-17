25f624a693bbae3e61ce35ebfbb7d0a16fb5bb9d
zfhjbvjd <107539576+zfhjbvjd@users.noreply.github.com>
2022-07-17 07:01:01 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vless_UUID="50c13e7f-9a7e-413d-a328-bcf79f50a407"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vmess_UUID="50c13e7f-9a7e-413d-a328-bcf79f50a407"
 fi
 echo ${Vmess_UUID}
 
